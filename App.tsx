50ef4eee59c7d53cdbd73c8eda0d09954e21967d
UrielP-Dev <kaattoo9@gmail.com>
2024-11-18 12:37:58 -0600

    Init Commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import "./global.css"
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text className='text-red-500'>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
